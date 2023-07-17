c97224eabe8a5c70db4b33530a099e45796d6ff0
rodolpop <garciasena@yahoo.com>
2023-07-17 18:44:26 +0200

    Contador año nuevo

---
+++ js/countdown.js
@@ -0,0 +1,34 @@
+
+
+
+simplyCountdown('#cuenta', {
+
+	year: 2024, 
+	month: 1, 
+	day: 1, 
+	hours: 1, 
+	minutes: 1, 
+	seconds: 1, 
+	words: { 
+		days: 'Día',
+		hours: 'Hora',
+		minutes: 'Minuto',
+		seconds: 'Segundo',
+		pluralLetter: 's'
+	},
+	plural: true, 
+	inline: false, 
+	inlineClass: 'simply-countdown-inline', 
+	
+	enableUtc: true, 
+	onEnd: function() {
+		document.getElementById('portada').classList.add('oculta');
+		return; 
+	}, 
+	refresh: 1000, 
+	sectionClass: 'simply-section', 
+	amountClass: 'simply-amount', 
+	wordClass: 'simply-word', 
+	zeroPad: false,
+	countUp: false
+});
